72b7d3e2465620c6a1d827148f5ec7a1107055a8
gyogy <gmkukov@gmail.com>
2021-10-13 16:45:20 +0300

    1.4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -23,9 +23,9 @@ const Part = (asdf) => {
 const Content = (args) => {
   return (
     <div>
-      <Part name={args.obj1.name} ex={args.obj1.exercises} />
-      <Part name={args.obj2.name} ex={args.obj2.exercises} />
-      <Part name={args.obj3.name} ex={args.obj3.exercises} />
+      <Part name={args.parts[0].name} ex={args.parts[0].exercises} />
+      <Part name={args.parts[1].name} ex={args.parts[1].exercises} />
+      <Part name={args.parts[2].name} ex={args.parts[2].exercises} />
     </div>
   )
 }
@@ -42,26 +42,26 @@ const Total = (args) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-
-    const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course_name={course} />
-      <Content obj1={part1} obj2={part2} obj3={part3}/>
-      <Total sum={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts}/>
+      <Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
     </div>
   )
 }
