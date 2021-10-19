900c61406558c86ad2670174068cc3a5139fcab4
gyogy <gmkukov@gmail.com>
2021-10-19 20:38:36 +0300

    1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -14,10 +14,13 @@ const App = () => {
       <button onClick={() => setBad(bad + 1)}>bad</button>
       
       <h2>statistics</h2>
-      <p></p>
+      
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good-bad) / (good + neutral + bad)}</p>
+      <p>positive {(good / (good + neutral + bad)) * 100} %</p> 
     </div>
   )
 }
